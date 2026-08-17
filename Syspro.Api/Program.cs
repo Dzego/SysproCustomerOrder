22090e2fffa17cd953adc869fde11b4c42d8a99e
Dzego <siseko9601@gmail.com>
2026-08-17 23:51:26 +0200

    Reorder AddControllers call in Program.cs for better clarity

--- Syspro.Api/Program.cs
+++ Syspro.Api/Program.cs
@@ -5,11 +5,11 @@ using Syspro.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+        
+builder.Services.AddControllers();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 builder.Services.AddScoped<ICustomerImportService, CustomerImportService>();
